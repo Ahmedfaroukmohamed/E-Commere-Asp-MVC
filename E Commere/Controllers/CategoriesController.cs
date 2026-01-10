c097650db3d393f951e277d7b42f49890c9155fd
Smart Laptop <Smart Laptop@DESKTOP-H00IARC>
2026-01-10 19:07:42 +0200

    End Controller Part

---
+++ E Commere/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using E_Commere.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commere.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public CategoriesController(EcommerceDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories =await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
---
+++ E Commere/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using E_Commere.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commere.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public ProductsController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var product = await _context.Products.ToListAsync();
+            return View(product);
+        }
+    }
+}
